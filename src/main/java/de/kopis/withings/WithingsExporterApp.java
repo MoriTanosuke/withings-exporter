018d0ca57f60670aff032cc026053928dcff94d2
Carsten Ringe <carsten@kopis.de>
2014-08-25 04:57:20 +0200

    Add command line --out to specify CSV file. Also add Jopt Simple for command line parsing.

--- src/main/java/de/kopis/withings/WithingsExporterApp.java
+++ src/main/java/de/kopis/withings/WithingsExporterApp.java
@@ -2,6 +2,8 @@ package de.kopis.withings;
 
 import com.google.gson.Gson;
 import de.kopis.withings.api.WithingsApi;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
 import org.eclipse.jetty.client.HttpClient;
 import org.eclipse.jetty.client.api.ContentResponse;
 import org.eclipse.jetty.server.Server;
@@ -10,7 +12,7 @@ import org.scribe.builder.ServiceBuilder;
 import org.scribe.model.*;
 import org.scribe.oauth.OAuthService;
 
-import java.io.StringReader;
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
@@ -29,13 +31,18 @@ public class WithingsExporterApp {
      */
     private final String YOUR_API_SECRET;
     /**
+     * Enable/disable debug logging.
+     */
+    private final boolean debug;
+    /**
      * Local jetty webserver to receive OAuth authorization callbacks.
      */
     private Server server;
 
-    public WithingsExporterApp(String apiKey, String apiSecret) {
+    public WithingsExporterApp(String apiKey, String apiSecret, boolean debug) {
         YOUR_API_KEY = apiKey;
         YOUR_API_SECRET = apiSecret;
+        this.debug = debug;
     }
 
     /**
@@ -43,19 +50,53 @@ public class WithingsExporterApp {
      *
      * @param args API key, API secret
      */
-    public static void main(String[] args) {
-        if (args == null || args.length != 2) {
-            System.err.println("Please provide API key and API secret as parameters.");
-            System.out.println("USAGE: java " + WithingsExporterApp.class.getName() + " <API_KEY> <API_SECRET>");
+    public static void main(String[] args) throws Exception {
+        OptionParser parser = new OptionParser();
+        //TODO make key+secret required arguments
+        parser.accepts("key", "you Withings API key").withRequiredArg().ofType(String.class).required();
+        parser.accepts("secret", "the secret to your Withings API key").requiredIf("key").withRequiredArg().ofType(String.class).required();
+        parser.accepts("out", "filename to write output to").withRequiredArg().ofType(String.class);
+        parser.accepts("range", "timerange to export, i.e. 30, 60, 90").withRequiredArg().ofType(Integer.class).defaultsTo(30);
+        parser.accepts("debug", "enable debug logging");
+        OptionSet options = null;
+        try {
+            options = parser.parse(args);
+        } catch (Exception e) {
+            System.err.println("Can not read options. Please check the provided options on your command line.");
+            parser.printHelpOn(System.out);
             System.exit(-1);
         }
-        new WithingsExporterApp(args[0], args[1]).export();
+
+        final String key = (String) options.valueOf("key");
+        final String secret = (String) options.valueOf("secret");
+        final Integer range = (Integer) options.valueOf("range");
+        final boolean debug = options.has("debug");
+
+        OutputStream out = null;
+        try {
+            if (options.has("out") && options.hasArgument("out")) {
+                final String filename = (String) options.valueOf("out");
+                out = new FileOutputStream(filename);
+            } else {
+                out = System.out;
+            }
+
+            new WithingsExporterApp(key, secret, debug).export(out, range);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            //TODO write to crashlog
+            e.printStackTrace();
+        } finally {
+            if (out != null) {
+                out.close();
+            }
+        }
     }
 
     /**
      * Starts an export of your Withings data via the API.
      */
-    private void export() {
+    private void export(OutputStream out, Integer timerange) throws IOException {
         OAuthService service = null;
         Token accessToken = null;
         Properties props = new Properties();
@@ -66,14 +107,17 @@ public class WithingsExporterApp {
         try {
             startJetty(jettyPort);
 
-            service = new ServiceBuilder()
-                    //.debug().debugStream(System.out)
+            ServiceBuilder serviceBuilder = new ServiceBuilder()
                     .provider(WithingsApi.class)
                     .apiKey(YOUR_API_KEY)
                     .apiSecret(YOUR_API_SECRET)
                     .signatureType(SignatureType.QueryString)
-                    .callback("http://localhost:" + jettyPort + "/callback")
-                    .build();
+                    .callback("http://localhost:" + jettyPort + "/callback");
+            if (debug) {
+                serviceBuilder.debug().debugStream(new FileOutputStream("debug.log"));
+            }
+            service = serviceBuilder.build();
+
             final Token requestToken = service.getRequestToken();
             final String authUrl = service.getAuthorizationUrl(requestToken);
 
@@ -117,8 +161,7 @@ public class WithingsExporterApp {
 
         final String userid = props.getProperty("userid");
 
-        int range = -30;
-        final String startdate = getDateFromRange(range);
+        final String startdate = getDateFromRange(-1 * timerange);
         final String today = getDateFromRange(0);
 
         // query activities in the given range
@@ -126,10 +169,12 @@ public class WithingsExporterApp {
 
         // convert from JSON into a simple Map for easy parsing
         // print out a temporary CSV
-        System.out.println("Date,Steps,Calories,Elevation");
+        BufferedWriter fw = new BufferedWriter(new OutputStreamWriter(out));
+        fw.append("Date,Steps,Calories,Elevation\n");
         for (Map body : (List<Map>) response.get("activities")) {
-            System.out.println(body.get("date") + "," + body.get("steps") + "," + body.get("calories") + "," + body.get("elevation"));
+            fw.append(body.get("date") + "," + body.get("steps") + "," + body.get("calories") + "," + body.get("elevation") + "\n");
         }
+        fw.flush();
 
         // check if jetty is still running
         if (server != null && server.isRunning()) {
@@ -180,6 +225,7 @@ public class WithingsExporterApp {
     }
 
     private void stopJetty() throws Exception {
+        //TODO server.join(); ?
         server.stop();
     }
 
